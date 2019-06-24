bb9e1b20afafd45d919929c79ff479e532eec9cc
Boris <51057023+bo90@users.noreply.github.com>
2019-06-24 19:43:37 +0300

    Добавьте файлы проекта.

---
+++ partsWinForm/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace partsWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 382);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Управление запчастями";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ partsWinForm/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+
+namespace partsWinForm
+{
+    public partial class Form1 : Form
+    {
+        private static string connstr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=repairDB.mdb;";
+        //repairDB.mdb
+        private OleDbConnection myConn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myConn = new OleDbConnection(connstr);
+            myConn.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConn.Close();
+        }
+
+        private void selectTest()
+        {
+            string query = "SELECT ID_Pair FROM Pairs WHERE id = 1";
+            OleDbCommand command = new OleDbCommand(query, myConn);
+            textBox1.Text = command.ExecuteScalar().ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            selectTest();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string qury = "SELECT id as '№', IndexPair as 'Парт №' FROM Pairs";
+            OleDbDataAdapter da = new OleDbDataAdapter(qury, myConn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, qury);
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddDetatil addDetatil = new AddDetatil();
+            addDetatil.Show();
+        }
+    }
+}
